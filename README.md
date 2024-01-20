7b153ea717972713306fbc83cbd365fe948ef676
Alex <format37@gmail.com>
2024-01-21 03:45:53 +0400

    modified:   README.md 	modified:   bot.py 	modified:   config.json

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Minecraft bot
 Mineflayer Langchain ReAct Agent  
 Based on [mineflayer](https://github.com/PrismarineJS/mineflayer/tree/master/examples/python) and [langchain](https://python.langchain.com)
-#### Features
-* Come to player  
-* Follow the player  
-* Stop following  
-* Search DuckDuckGo  
-* Search Wikipedia  
-* Retrieval of text files, stored in the "context" folder
+#### Commands examples
+* Janet, come to me
+* Janet, follow me  
+* Janet, stop  
+* Janet, what the weather tomorrow in London?  
+* Janet, i am looking for iron_ore  
+* Janet, search in kniowledge base the coordinates of home
 #### Requirements
 * OpenAI API key
 * Minecraft running server
--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import json
 import logging
 from pydantic import BaseModel, Field
-from langchain.agents import initialize_agent
+from langchain.agents import Tool, initialize_agent
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -12,9 +12,11 @@ from langchain_community.tools import DuckDuckGoSearchRun
 from langchain.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from javascript import require, On
+from langchain.chains import RetrievalQA
 import time as py_time
 # from langchain.globals import set_debug
 # set_debug(True)
+
 
 # Define the info logger
 logger = logging.getLogger(__name__)
@@ -28,7 +30,7 @@ logger.addHandler(handler)
 
 mineflayer = require('mineflayer')
 pathfinder = require('mineflayer-pathfinder')
-
+blockfinder = require('mineflayer-blockfinder')(mineflayer)
 
 class ConfigLoader:
     def __init__(self, config_filename='config.json'):
@@ -42,6 +44,13 @@ class ConfigLoader:
         with open(self.config_filename, 'r') as file:
             return json.load(file)
 
+temp_config = ConfigLoader('config.json').config
+mcdata = require('minecraft-data')(temp_config['minecraft']['version'])
+
+# Save block list to a file
+"""with open(f"{temp_config['context']['path']}/blocks.txt", 'w') as file:
+    for block in mcdata.blocksArray:
+        file.write(block.name + '\n')"""
 
 class DocumentProcessor:
     def __init__(self, config):
@@ -55,7 +64,12 @@ class DocumentProcessor:
         text_splitter = RecursiveCharacterTextSplitter()
         documents = text_splitter.split_documents(docs)
         vector = DocArrayInMemorySearch.from_documents(documents, embeddings)
+        print(f"### Vector processed!")
         return vector.as_retriever()
+
+
+class DocumentInput(BaseModel):
+    question: str = Field()
 
 
 class ChatAgent:
@@ -75,16 +89,29 @@ class ChatAgent:
 
         tools = [self.create_structured_tool(func, name, description, return_direct)
                  for func, name, description, return_direct in [
-                     (self.bot_instance.bot_action_come, "Command to come to Minecraft player",
-                      "Provide the name of the player asking to come", True),
-                     (self.bot_instance.bot_action_follow, "Command to follow for Minecraft player",
-                      "Provide the name of the player asking to follow", True),
-                     (self.bot_instance.bot_action_stop, "Command to stop performing any actions in Minecraft",
-                      "You may provide the name of player asking to stop", True)]
+                        (self.bot_instance.bot_action_come, "Command to come to Minecraft player",
+                            "Provide the name of the player asking to come", True),
+                        (self.bot_instance.bot_action_follow, "Command to follow for Minecraft player",
+                            "Provide the name of the player asking to follow", True),
+                        (self.bot_instance.bot_action_stop, "Command to stop performing any actions in Minecraft",
+                            "You may provide the name of player asking to stop", True),
+                        (self.bot_instance.bot_action_take, "Command to take an item in Minecraft",
+                            "Provide the name of the item to take", True),
+                        (self.bot_instance.bot_action_find, "Command to find an item in Minecraft",
+                            "Provide the name of the item to find", True)
+                      ]
                  ]
         tools.append(DuckDuckGoSearchRun())
-        wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
-        tools.append(wikipedia)
+        # wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+        # tools.append(wikipedia)
+        tools.append(
+            Tool(
+                args_schema=DocumentInput,
+                name='Knowledge base',
+                description="Providing a game information from the knowledge base",
+                func=RetrievalQA.from_chain_type(llm=llm, retriever=self.retriever),
+            )
+        )
 
         return initialize_agent(
             tools,
@@ -108,19 +135,23 @@ class ChatAgent:
 
 class Bot:
     RANGE_GOAL = 1
-    # BOT_USERNAME = 'Janet'
 
     def __init__(self):
         self.player_to_follow = None
         self.chat_history = []
         self.config = ConfigLoader('config.json').config
+        self.document_processor = DocumentProcessor(self.config)
+        self.retriever = self.document_processor.process_documents()
         
         self.bot = mineflayer.createBot({
             'host': self.config['minecraft']['host'],
             'port': self.config['minecraft']['port'],
             'username': self.config['minecraft']['username'],
         })
+        if not self.bot._client.version == self.config['minecraft']['version']:
+            print(f"Bot version: {self.bot._client.version} is not equal to config version: {self.config['minecraft']['version']}")
         self.bot.loadPlugin(pathfinder.pathfinder)
+        self.bot.loadPlugin(blockfinder)
         logger.info("Started mineflayer")
         # Bind event handlers to the bot instance
         @On(self.bot, 'spawn')
@@ -147,10 +178,8 @@ class Bot:
             logger.info("Not a message for me")
             return
         self.chat_history = [] # Chat history have no benefits yet
-        document_processor = DocumentProcessor(self.config)
-        retriever = document_processor.process_documents()
 
-        chat_agent = ChatAgent(self.config, retriever, self)
+        chat_agent = ChatAgent(self.config, self.retriever, self)
 
         user_input = f"Player: {sender}. Message: {message}"
         logger.info(f'sending:\n{user_input}')
@@ -204,6 +233,49 @@ class Bot:
         self.player_to_follow = None
         self.bot.pathfinder.setGoal(None)        
         return f'I have stopped'
+    
+    def bot_action_take(self, val: str) -> str:
+        self.bot.equip(val)
+        return f'Equipping {val}'
+    
+    def bot_action_find(self, val: str) -> str:
+        self.bot.chat(f"Finding {val}")
+        def on_find(err, blockPoints):
+            logger.info(f"on_find call")
+            if err:
+                self.bot.chat(f'Error trying to find the chosen block: {err}')
+                # self.bot.quit('quitting')
+            elif blockPoints:
+                self.bot.chat(f'I found a mentioned block at {blockPoints[0].position}.')
+                # Move to the block
+                self.bot.pathfinder.setMovements(pathfinder.Movements(self.bot))
+                self.bot.pathfinder.setGoal(pathfinder.goals.GoalBlock(blockPoints[0].position.x, blockPoints[0].position.y, blockPoints[0].position.z))
+            else:
+                self.bot.chat("I couldn't find any mentioned blocks within 256.")
+                # self.bot.quit('quitting')
+        # Check if the block exists
+        """if not hasattr(mcdata.blocksByName, val):
+            message = f'There is no block named {val}.'
+            self.bot.chat(message)
+            return message"""
+
+            # self.bot.quit('quitting')
+        try:
+            block_id = eval(f'mcdata.blocksByName.{val}.id')
+        except Exception as e:
+            message = f'There is no block named {val}.'
+            self.bot.chat(message)
+            return message
+        
+        self.bot.chat(f'Block id {block_id}')
+        self.bot.findBlock({
+            'point': self.bot.entity.position,
+            'matching': block_id,
+            'maxDistance': 256,
+            'count': 1
+        }, on_find)
+
+        return f'Finding {val}'
 
 
 class BotActionType(BaseModel):
